d6c94bd38b123186affe89b427a5554314ef91f1
Ionuț Oceanu <oceanu.ionut@yahoo.ro>
2020-03-10 05:56:05 +0200

    updating interface for v2 and adding functionalities

--- scriptv2.js
+++ scriptv2.js
@@ -1,3 +1,25 @@
+//vars
+	let card;
+	let allCards = [];
+	let playerHand = [];
+	let playerHandAfterFlop = [];
+	let dealerHand = [];
+	let handValuePlayer = 0;
+	let handValueDealer = 0;
+	let handValuePlayerWithA = 0;
+	let handValueDealerWithA = 0;
+	let cardAux = 'c5';
+  	let container = 'container';
+
+// alert("For a better experience, prees F11 to enter in full screen mode");
+// A
+// ||
+// ||
+// ||
+// ||
+// ||
+
+// function that choose a random card
 function shot() {
 	let counter = 0;
 	let shootACard = Math.floor((Math.random() * 52) + 1);
@@ -168,3 +190,125 @@ function shot() {
 	  	 return "something went wrong";
 	}
 };
+
+//assure that will be no duplicate cards during a game
+function checkDuplicate() {
+	for (i =0; i< allCards.length; i++) {
+		if (allCards[i] === card){
+	card = shot();
+	i = 0;	
+	}
+  }
+}
+
+//hit 2 cards for each player
+function dealResult(){
+	hitCard(playerHand, 1, 'c1');
+	hitCard(playerHand, 1, 'c2');
+	hitCard(playerHand, 1, 'c3');
+	hitCard(playerHand, 1, 'dealerHidenCard');  //security issue. can be seen from console 
+	console.log("allCards",allCards,"\n", "player's hand",playerHand,"\n", "dealer's hand",dealerHand);
+}
+
+//this function hit a card and check to don't exist a duplicate and assign it to player / dealer and to 
+//              allCard array for future check for duplicates
+// 1st param - player / dealer Hand
+//2nd param - *times the hit card function will run 
+function hitCard(who, number, element) {
+	for (var i = 0; i < number; i++) {	
+		card = shot();
+		checkDuplicate();
+		// card1Player = card;
+	  	allCards.push(card);
+	  	who.push(card);
+	}
+	console.log(card);
+	return document.getElementsByClassName(element)[0].innerHTML = card;
+}
+
+//this function calculate the sum of hand
+// 1st param - player / dealer hand
+//2nd param - var where the result of sum will be stored 
+//3rd param - var where the result of sum will be stored in case of A is shot	
+function sumOfHand(whosHand, whosValue,varWithA,htmlElem) {
+	
+	for (var i = 0; i < whosHand.length; i++) {
+		 let handValue = whosHand[i].charAt(0);
+		 // console.log(whosHand[i].charAt(0));
+		 if (handValue > 1 && handValue <= 9) {
+		 	handValue = parseInt(handValue)
+		 	whosValue = whosValue + handValue;
+		 	varWithA = varWithA + handValue;
+		 } else if (handValue === "J" || handValue === "Q" || handValue === "K" || handValue === "1" || handValue === "A") {
+		 	if (handValue === "A") {
+		 		varWithA = varWithA + 1;
+		 		whosValue = whosValue + 11;
+		 	} else {
+
+		 	handValue = 10;
+		 	varWithA = varWithA + handValue; 
+		 	whosValue = whosValue + handValue;
+		 	}		 		
+		 }
+		 };
+		 // document.getElementById(htmlElem).innerHTML(whosHand.value);
+		 console.log(whosHand);
+		 if (whosValue > 21 && varWithA > 21) {
+		 	document.getElementById(htmlElem).innerHTML= "Busted!";
+		 	document.getElementById('pScoreNum').style.backgroundColor='red';
+		 	document.getElementById('pScoreNum').style.textAlign='center';
+			stopGame();
+		 	btnReset();
+		 } else if (whosValue === 21 || varWithA === 21) {
+		 	stopGame();
+		 	document.getElementById(htmlElem).innerHTML= "BlackJack!";
+		 } else if (whosValue > 21 && varWithA != 21) {
+		 	document.getElementById(htmlElem).innerHTML= varWithA;
+		 }  else if (varWithA < whosValue) {
+		 	document.getElementById(htmlElem).innerHTML= whosValue + `/` + varWithA;
+		 } else {
+		 	document.getElementById(htmlElem).innerHTML= whosValue;
+		 }
+	return whosValue; 	 
+		
+}
+
+// this function will "start" the game
+// hide start button
+// show the other in games buttons
+// deal & display cards
+
+  function startGame() {	
+    document.getElementsByClassName("bhit")[0].style.display = "inline-block";
+    document.getElementsByClassName("bstop")[0].style.display = "inline-block";
+    document.getElementsByClassName("bstart")[0].style.display = "none";
+    
+    document.getElementsByClassName("pcard")[0].style.display = "inline";
+    document.getElementsByClassName("pcard")[1].style.display = "inline";
+    document.getElementsByClassName("pcard")[2].style.display = "inline";
+    document.getElementsByClassName("pcard")[3].style.display = "inline";
+
+    dealResult();
+    score();
+  };
+
+    function score() {
+  		handValuePlayer = sumOfHand(playerHand, handValuePlayer,handValuePlayerWithA,'pScoreNum');
+  };
+
+  function stopGame() {
+			document.getElementsByClassName("bhit")[0].style.display = "none";
+		    document.getElementsByClassName("bstop")[0].style.display = "none";
+};
+
+function btnReset() {
+	document.getElementsByClassName('breset')[0].style.display='block';
+};
+
+function reload(){
+	location.reload();
+}
+
+  function delay() {
+
+  };
